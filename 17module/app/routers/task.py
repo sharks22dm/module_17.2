d4667180b1309b7bc9a9111d972f815262880eb3
sharks22dm <165840551+sharks22dm@users.noreply.github.com>
2024-09-12 12:32:25 +0300

    Add files via upload

---
+++ 17module/app/routers/task.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from app.backend.db_depends import get_db
+from typing import Annotated
+from app.models import *
+from app.schemas import CreateUser, UpdateUser
+from sqlalchemy import insert, select, update, delete
+from slugify import slugify
+
+router = APIRouter(prefix='/task', tags=['task'])
+
+
+@router.get('/')
+async def all_tasks():
+    pass
+
+
+@router.get('/task_id')
+async def task_by_id():
+    pass
+
+
+@router.post('/create')
+async def create_task():
+    pass
+
+
+@router.put('/update')
+async def update_task():
+    pass
+
+
+@router.delete('/delete')
+async def delete_task():
+    pass
